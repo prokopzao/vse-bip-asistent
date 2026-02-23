a176a407253ac1b6ea7b475ebfb3412b267510e6
prokopzao <prokopzaoral@seznam.cz>
2026-02-24 00:25:20 +0100

    Update app.py

--- app.py
+++ app.py
@@ -180,7 +180,7 @@ try:
     
     # Zkusíme použít specifický název pro stabilní kanál
     # Pokud ani toto nepomůže, zkus 'gemini-1.5-flash-latest'
-    model = genai.GenerativeModel('model: "gemini-2.0-flash')
+    model = genai.GenerativeModel('gemini-1.5-pro-latest')
 
     # ... zbytek kódu s historií a chatem ...
 
@@ -205,6 +205,7 @@ try:
 
 except Exception as e:
     st.error(f"AI se právě restartuje. (Chyba: {e})")
+
 
 
 
